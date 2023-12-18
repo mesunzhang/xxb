5df0cfeec87b3728a3882cd7877b1e43d0cddd79
zhangyutian <443083118@qq.com>
2023-12-18 16:04:50 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,17 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { ConfigProvider,Button, Form, Field, CellGroup } from 'vant';
+import 'vant/lib/index.css';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ConfigProvider)
+app.use(Button)
+app.use(Form);
+app.use(Field);
+app.use(CellGroup);
 
 app.mount('#app')
